6f886bb5d7d83604ac620d28cf1ae061fef0bb39
Joao <joao.ribeiro190404@gmail.com>
2024-06-09 17:56:15 -0300

    mudei a frase

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("qualquer coisa");//segundo commit
+        System.out.println("asda");//segundo commit
 
     }
 }
