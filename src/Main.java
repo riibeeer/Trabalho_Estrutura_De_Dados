c4007fa3d72b1be92168837c8010128f0f502679
AAndreluis-dev <aandreluis.a.a@gmail.com>
2024-06-12 01:45:50 -0300

    update program

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,29 @@
+import DB.ClientesDB;
+import DB.ProdutoDB;
+import entities.Cliente;
+import entities.Produto;
+import entities.Vendedor;
+import menu.Menu;
+
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("asda");//segundo commit
+        //Cadastro previo produto
+        Vendedor mercadoLivre = new Vendedor("Mercado Livre", "000000", "mercadolivre@gmail.com", "mercado");
+        ProdutoDB.addProduto(new Produto("Caneca do batman", "Uma caneca ilustrada com o batman", 12.90, 20, mercadoLivre));
+        ProdutoDB.addProduto(new Produto("Chinelo", "Um chinelo tamanho M", 5, 50, mercadoLivre));
+
+        //Cadastro Usuario
+        ClientesDB.adicionarCliente(new Cliente("riber", "riber2", "123", "12345"));
+
+        //Cadastrar Vendedor
+        ClientesDB.adicionarCliente(new Vendedor("Paty", "paty@", "paty2", "1234"));
+
+        Menu.menuPrincipal();
 
     }
 }
